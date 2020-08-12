314a493375604e759b7fe678ddb81734714e091f
MiXProjecT <Vovan4ik112@gmail.com>
2020-08-12 14:33:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:8081"
+    origin: "https://pizza-delivery-t.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
